e694055e9195347e2e544cf4a6f8585fedf75d52
ikustow <ikustow@yandex.run>
2022-08-28 03:27:35 +0300

    Firebase auth added
    Signed-off-by: ikustow <ikustow@yandex.run>

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -7,43 +9,45 @@ import 'auth/pages/auth_checker.dart';
 import 'auth/pages/error_screen.dart';
 import 'auth/pages/loading_screen.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations(
-          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
-      .whenComplete(() {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-        overlays: [SystemUiOverlay.bottom]);
+/*
+  if (Platform.isIOS) {
+    if (Firebase.apps.isNotEmpty) {
+      await Firebase.initializeApp();
+      */
+/*await Firebase.initializeApp(
+          options: FirebaseOptions(
+              apiKey: "AIzaSyAHjQpCho10o_MP15WDP5s1kqEyXcDa-QE<",
+              appId: "1:837271975898:ios:8475cf0d1c7af4913b32e3",
+              messagingSenderId: "837271975898",
+              projectId: "game-949a6"));*//*
 
-    runApp(
-      const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: MyApp(),
-      ),
-    );
-  });
+    } else {
+
+    }
+*/
+
+    //
+  
+  await Firebase.initializeApp();
+  runApp(ProviderScope(child: MyApp()));
 }
 
+/*//  This is a FutureProvider that will be used to check whether the firebase has been initialized or not
 final firebaseinitializerProvider = FutureProvider<FirebaseApp>((ref) async {
   return await Firebase.initializeApp();
-});
+});*/
 
 class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context, WidgetRef ref) {
 
-
-    final initialize = ref.watch(firebaseinitializerProvider);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
 
-         home: initialize.when(
-          data: (data) {
-            return const AuthChecker();
-          },
-          loading: () => const LoadingScreen(),
-          error: (e, stackTrace) => ErrorScreen(e, stackTrace)),
+         home: AuthChecker()
     );
   }
 }
